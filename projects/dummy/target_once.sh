c5e2b06c5ffd07f55ad48c68d68a099fc49617ac
Louis Lo <yjlou.tw@gmail.com>
2024-10-06 17:47:09 -0700

    projects/dummy/target_once.sh: rename target_setup_pre_project_packages()

--- projects/dummy/target_once.sh
+++ projects/dummy/target_once.sh
@@ -10,7 +10,7 @@ cd "$(dirname "$0")"
 . rpi_installer/target_setup.sh
 . shflags
 
-target_setup_pre_project_packages() {
+target_setup_project_packages() {
   apt install -y xxd curl python3 python3-pip
 }
 
@@ -20,13 +20,13 @@ target_setup_parse_args "$@"
 eval set -- "${FLAGS_ARGV}"
 
 if [ ${FLAGS_project_only} -eq ${FLAGS_TRUE} ]; then
-  target_setup_pre_project_packages
+  target_setup_project_packages
 else
   target_setup_post_ssh_enable  # turn on the SSH for debug.
   target_setup_pre_time_is_synced
   target_setup_pre_locale_and_keyboard
   target_setup_pre_common_packages
-  target_setup_pre_project_packages
+  target_setup_project_packages
   target_setup_post_networks
   target_setup_post_ssh_enable
   target_setup_post_shutdown
